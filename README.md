4379f1a856fe77631859efc5b933e4152f174365
Fede <federicoalvarez710@gmail.com>
2021-03-08 15:09:14 -0300

    add vue 3 template

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# pagofacil-onboarding-vue3
+# vue3-router-typescript-pug-base
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template lang=pug>
+.home
+  img(alt="Vue logo", src="../assets/logo.png")
+  hello-world(msg="Welcome to Your Vue.js + TypeScript App")
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+
+@Options({
+  components: {
+    HelloWorld,
+  },
+})
+export default class Home extends Vue {}
+</script>

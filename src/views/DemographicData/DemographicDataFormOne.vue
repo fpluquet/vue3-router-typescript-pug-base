3701a8cb516b2dd5333e86746cb3d7f2bdcdaa65
Fede <federicoalvarez710@gmail.com>
2021-03-17 11:38:41 -0300

    fix

--- src/views/DemographicData/DemographicDataFormOne.vue
+++ src/views/DemographicData/DemographicDataFormOne.vue
@@ -56,7 +56,7 @@ export default {
     let messageError = ref("");
     let loading = ref(false);
 
-    const saveNF = () =>
+    const saveNF = () => {};
     return {
       saveNF,
       formData,

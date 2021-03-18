a6deadc042f50182124b3dadcb0940f7289407d7
Fede <federicoalvarez710@gmail.com>
2021-03-18 00:27:19 -0300

    create button color component

---
+++ src/components/ButtonColor.vue
@@ -0,0 +1,44 @@
+<template>
+  <button :type="type" class="button custom-button" :class="accountType">
+    <slot />
+  </button>
+</template>
+
+<script>
+import { ref, computed } from "vue";
+import { useStore } from "vuex";
+
+export default {
+  name: "Button",
+  props: {
+    type: String,
+    className: String,
+  },
+  setup() {
+    let accountType = ref(null);
+    const store = useStore();
+    accountType = computed(() => store.state.account.type);
+    return { accountType };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.custom-button {
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 16px;
+  text-align: center;
+  color: #ffffff !important;
+  min-height: 42px;
+  border-radius: 0px;
+  &.persona {
+    background-color: #1d9add;
+  }
+  &.empresa {
+    background-color: #6ac24b;
+  }
+}
+</style>

1a890b71991f09b673e069eea91064594d059738
Fede <federicoalvarez710@gmail.com>
2021-03-22 15:53:55 -0300

    change routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,9 +2,10 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Authenticated from '../components/Authenticated.vue'
 import Home from '../views/Home.vue'
 import Dashboard from '../views/Dashboard/Dashboard.vue'
-import AccountForm from '../views/AccountForm.vue'
+import AccountForm from '../views/Account/AccountForm.vue'
 import ContinueRegister from '../views/Account/ContinueRegister.vue'
-import SelectAccountType from '../views/SelectAccountType.vue'
+import CodeInput from '../views/Account/CodeInput.vue'
+import SelectAccountType from '../views/Account/SelectAccountType.vue'
 import DemographicData from '../views/DemographicData/index.vue'
 import BiometricValidation from '../views/BiometricValidation.vue'
 import Documentation from '../views/Documentation.vue'
@@ -26,19 +27,24 @@ const routes: Array<RouteRecordRaw> = [
     redirect: { name: 'select-account-type' },
     children: [
       {
-        path: 'select-account-type',
+        path: 'account-section/select-account-type',
         name: 'select-account-type',
         component: SelectAccountType
       },
       {
-        path: 'create-account/:accountType(persona|empresa)',
+        path: '/account-section/create-account/:accountType(persona|empresa)',
         name: 'create-account',
         component: AccountForm,
       },
       {
-        path: '/continue-register',
+        path: '/account-section/continue-register',
         name: 'continue-register',
         component: ContinueRegister,
+      },
+      {
+        path: '/account-section/code-input',
+        name: 'code-input',
+        component: CodeInput,
       },
     ]
   },

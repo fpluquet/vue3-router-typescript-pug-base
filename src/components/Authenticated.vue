ec359164a0ee250175df810b2bd9ccdbb156350e
Fede <federicoalvarez710@gmail.com>
2021-03-23 14:51:29 -0300

    manage route based on cognitoId

--- src/components/Authenticated.vue
+++ src/components/Authenticated.vue
@@ -7,12 +7,12 @@ import { ref, onMounted, computed } from "vue";
 import { useStore } from "vuex";
 import { useRouter } from "vue-router";
 export default {
+  name: "Authenticated",
   setup() {
     const store = useStore();
     const router = useRouter();
-    let cognitoId = ref(false);
     onMounted(() => {
-      cognitoId = Boolean(store.state.account.cognitoId);
+      let cognitoId = Boolean(store.state.account.cognitoId);
       if (cognitoId) {
         router.push({
           name: "Dashboard",
@@ -23,7 +23,7 @@ export default {
       }
     });
 
-    return { cognitoId };
+    return {};
   },
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,12 +3,27 @@
 </template>
 
 <script>
+import { onMounted } from "vue";
+import { useStore } from "vuex";
+import { useRouter } from "vue-router";
 import Splash from "../views/Splash.vue";
 export default {
   name: "Home",
   components: {
     Splash,
   },
-  setup() {},
+  setup() {
+    const store = useStore();
+    const router = useRouter();
+    onMounted(() => {
+      let cognitoId = Boolean(store.state.account.cognitoId);
+      if (cognitoId) {
+        router.push({
+          name: "Dashboard",
+          params: { accountType: "persona" },
+        });
+      }
+    });
+  },
 };
 </script>
